6a787cfff5959c0b0afbe6575e9efa37b586b631
Aditya Trivedi <adityatrivedi128@outlook.com>
2019-09-05 22:00:12 +0530

    Added database dump file to the app

---
+++ compacturl-dumpfile.sql
@@ -0,0 +1,77 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 9.5.19
+-- Dumped by pg_dump version 9.5.19
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
+--
+
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+
+
+--
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: shortnames; Type: TABLE; Schema: public; Owner: vagrant
+--
+
+CREATE TABLE public.shortnames (
+    name text NOT NULL,
+    url text
+);
+
+
+ALTER TABLE public.shortnames OWNER TO vagrant;
+
+--
+-- Data for Name: shortnames; Type: TABLE DATA; Schema: public; Owner: vagrant
+--
+
+COPY public.shortnames (name, url) FROM stdin;
+\.
+
+
+--
+-- Name: shortnames_pkey; Type: CONSTRAINT; Schema: public; Owner: vagrant
+--
+
+ALTER TABLE ONLY public.shortnames
+    ADD CONSTRAINT shortnames_pkey PRIMARY KEY (name);
+
+
+--
+-- Name: SCHEMA public; Type: ACL; Schema: -; Owner: postgres
+--
+
+REVOKE ALL ON SCHEMA public FROM PUBLIC;
+REVOKE ALL ON SCHEMA public FROM postgres;
+GRANT ALL ON SCHEMA public TO postgres;
+GRANT ALL ON SCHEMA public TO PUBLIC;
+
+
+--
+-- PostgreSQL database dump complete
+--
+
